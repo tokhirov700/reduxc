cccaf7502b18f98f4bb843fc4b49732747cc7912
Sarvar <162941642+tokhirov700@users.noreply.github.com>
2024-07-19 04:08:28 +0500

    3 work

--- src/redux/reducers/reducer.js
+++ src/redux/reducers/reducer.js
@@ -6,7 +6,6 @@ const reducer = (state = initialState, action) => {
     switch (action.type) {
         case "CHANGE_EMAIL":
             return {
-                ...state,
                 email: action.email
             }
         default:
